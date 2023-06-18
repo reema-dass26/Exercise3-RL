c0c2513858b7a9af90f1d0ee91dc2b7f723e00b6
Valentin Schweitzer <valentin.schweitzer98@gmail.com>
2023-06-18 13:42:44 +0200

    Add option to change brick color

--- brick.py
+++ brick.py
@@ -4,17 +4,17 @@ BLACK = (0, 0, 0)
 
 
 class Brick(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, color = (0,0,0)):
         # super().__init__()
         pygame.sprite.Sprite.__init__(self)
         
         # Create an image of the paddle
         self.image = pygame.Surface([width, height])
-        self.image.fill(BLACK)
+        self.image.fill(color)
         # Scale the image to fit the grid cell size
         #self.image = pygame.transform.scale(self.image, (pixel_scale, pixel_scale))
         
-        pygame.draw.rect(self.image, BLACK, [0, 0, width, height])
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.rect = self.image.get_rect()
         
         self.speed=0
